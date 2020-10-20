6cddb93276104b1f696acd70966339b433d34bf1
BeomGeun Shin <beomgeunshin@BeomGeunui-MacBookAir.local>
2020-10-20 14:54:53 +0900

    modified webpack setting

--- server/index.ts
+++ server/index.ts
@@ -4,23 +4,18 @@ import bodyParser from 'body-parser';
 import router from './routes/api'
 import path from 'path';
 import {bumblog} from './config/bumblog.config';
-
-// const config = require('./config/private.config');
-// const morgan = require('morgan');
+import morgan from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 8001;
 
-let buildDir = path.resolve(__dirname, '../build'); // 개발 모드
+let buildDir = path.resolve(__dirname, '../build');
 
 if(process.env.NODE_ENV === 'production'){ // 배포 모드
-    console.log('배포 모드');
     buildDir = path.resolve(__dirname, './build');
 }
 
-console.log('buildDir', buildDir);
-
-// app.use(morgan('dev'));
+app.use(morgan('dev')); // 로그 기록
 app.set('jwt-secret', bumblog.secret);
 
 app.use(bodyParser.urlencoded({
@@ -28,20 +23,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-// console.log('ddsdsaad', path.join(buildDir, "index.html"));
-// app.use("/*", express.static(buildDir));
-// app.get("/*", (req:Request, res:Response) =>{
-//     res.sendFile(path.join(buildDir, "index.html"));
-// });
-
 app.use('/', express.static(buildDir));
 app.use('/board', express.static(buildDir));
 app.use('/board/view', express.static(buildDir));
 app.use('/board/write', express.static(buildDir));
-
 app.use('/api', router);
 
-//Express 서버 완료시
+// Express 서버 완료시
 app.listen(port, ()=>{
     console.log(`[express] Express server is running on port ${port}`)
 });
@@ -52,7 +40,9 @@ db.on('error', console.error);
 db.once('open', ()=>{
     console.log('[mongoose] connected to mongodb server');
 });
-mongoose.set('debug', true);
+if(process.env.NODE_ENV !== 'production'){
+    mongoose.set('debug', true); // 개발 모드
+}
 mongoose.connect(bumblog.mongodbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
